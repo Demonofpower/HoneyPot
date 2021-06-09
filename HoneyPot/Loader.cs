ab2bf6e085a3edfbcce12e31381e8eadcec74899
Paranoia <9366819+Demonofpower@users.noreply.github.com>
2021-06-09 09:53:43 +0200

    idk

---
+++ HoneyPot/Loader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HoneyPot
+{
+    public class Loader
+    {
+        public static void Load()
+        {
+            _gameObject = new GameObject();
+            _gameObject.AddComponent<Paranoia>();
+            GameObject.DontDestroyOnLoad(_gameObject);
+        }
+        public static void Unload()
+        {
+            _Unload();
+        }
+        private static void _Unload()
+        {
+            GameObject.Destroy(_gameObject);
+        }
+        private static GameObject _gameObject;
+    }
+}
---
+++ HoneyPot/Paranoia.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace HoneyPot
+{
+    public class Paranoia : MonoBehaviour
+    {
+        public void Start()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void OnGUI()
+        {
+            GUI.contentColor = Color.magenta;
+            GUI.Label(new Rect(0f, 30f, 150f, 50f), "HoneyPot");
+            GUI.contentColor = Color.white;
+            GUI.Label(new Rect(0f, 50f, 150f, 50f), "Press F1 for Menu");
+            GUI.contentColor = Color.red;
+            GUI.Label(new Rect(0f, 70f, 150f, 50f), "By Paranoia with <3");
+        }
+    }
+}
